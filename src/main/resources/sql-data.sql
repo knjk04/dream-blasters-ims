d037473c3c1027685dd9b0259ebf558f58f904af
sultanm12 <33058560+sultanm12@users.noreply.github.com>
2021-12-03 12:23:02 +0000

    Update sql-data.sql

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,10 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Jordan', 'Harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Leeroy', 'Jenkins');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Ronnie', 'Pickering');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Joseph ', 'Harding');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('James', 'Dyson');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Steve', 'Jobs');
+
+
+
+
